de35c5dc82a5bedf1aa6c1f25d9139243ead3c42
George Hadjidemetriou <georgehadjidemetriou90@gmail.com>
2020-05-21 21:03:27 +0200

    - Replaced Jade with Handlebars - Added test api - Added /home redirect

--- app.js
+++ app.js
@@ -1,41 +1,38 @@
 var createError = require('http-errors');
+var cookieParser = require('cookie-parser');
+
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const exphbs = require('express-handlebars')
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const PORT = 5000;
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// get all todos
+app.get('/test', (req, res) => {
+  res.status(200).send({
+    success: 'true',
+    data: 'this was a successful test!'
+  })
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.get('/home', (request, response) => {
+  response.render('home', {
+    name: 'C. Pot... I\'ve been expecting you...'
+  })
+})
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.listen(PORT, () => {
+  console.log('server running on port ${PORT}')
 });
 
 module.exports = app;
